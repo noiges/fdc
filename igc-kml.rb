3584409aa3c6c482532c2f1d649d70b0e2e234cc
Tobias Noiges <tobias.noiges@gmail.com>
2012-05-16 11:14:15 +0200

    Fix wrong track tag case

--- igc-kml.rb
+++ igc-kml.rb
@@ -16,7 +16,7 @@ module Location
     long_dec = long[1].to_f + (long[2].to_f / 1000 / 60)
     long_dec *= (-1) if long[5] == "W"
     
-    dec = [long_dec, lat_dec]
+    dec = [long_dec.to_f, lat_dec.to_f]
 
   end
 
@@ -63,18 +63,19 @@ class Converter
     xml.instruct!
     xml.kml "xmlns" => "http://www.opengis.net/kml/2.2", "xmlns:gx" => "http://www.google.com/kml/ext/2.2" do
       xml.Document { 
-        xml.name "Test"
+        # xml.name "Test"
         xml.Placemark {
-          xml.name "Test"
-          xml.gx:track do
+          # xml.name "Test"
+          xml.gx:Track do
             xml.altitudeMode "absolute"
             @b_records.each do |b_record|
-              time = DateTime.new(2000 + @date[4].to_i, @date[3].to_i, @date[2].to_i, 
-                b_record[1].to_i, b_record[2].to_i, b_record[3].to_i)
-              coords = Location.to_dec(b_record[4..5]) << b_record[8]
-            
-              xml.when time
-              xml.gx :coord, coords.join(" ")
+               time = DateTime.new(2000 + @date[4].to_i, @date[3].to_i, @date[2].to_i, 
+                 b_record[1].to_i, b_record[2].to_i, b_record[3].to_i)
+               xml.when time
+            end
+            @b_records.each do |b_record|
+               coords = Location.to_dec(b_record[4..5]) << b_record[8].to_f
+               xml.gx :coord, coords.join(" ")
             end
           end
         }
@@ -87,7 +88,7 @@ class Converter
 
 end
 
-converter = Converter.new("sample/25EXXXX2.igc")
+converter = Converter.new("sample/25GXXXX1.igc")
 
 file = File.new("test.kml", "w")
 file.write(converter.kml)
