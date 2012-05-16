205c76ea948911e995bc04d2ffe667e8dffef984
Tobias Noiges <tobias.noiges@gmail.com>
2012-05-16 22:41:12 +0200

    Add cli tests

--- test/tc_cli.rb
+++ test/tc_cli.rb
@@ -16,14 +16,34 @@ class TestCLI < Test::Unit::TestCase
     system("ruby ../igc-kml.rb .")
     assert($?.exitstatus == 254)
     
+    system("ruby ../igc-kml.rb -s data/25GXXXX2")
+    assert($?.exitstatus == 253)
+    
+    system("ruby ../igc-kml.rb -s data/25GXXXX2.icg")
+    assert($?.exitstatus == 253)
+    
+    system("ruby ../igc-kml.rb -s data/25GXXXX2_äöü-")
+    assert($?.exitstatus == 253)
+    
+    system("ruby ../igc-kml.rb -s data/25GXXXX2.kml")
+    assert($?.exitstatus == 253)
+  
+  end
+  
+  def test_file_names
+    
     system("ruby ../igc-kml.rb data/25GXXXX1.igc")
     assert($?.exitstatus == 0)
     
-    system("ruby ../igc-kml.rb data/25GXXXX1_corrupt.igc")
-    assert($?.exitstatus == 253)
+    system("ruby ../igc-kml.rb data/25GXXXX1")
+    assert($?.exitstatus == 0)
     
-    # TODO: Test file name independence
-  
+    system("ruby ../igc-kml.rb data/25GXXXX1_äöü-")
+    assert($?.exitstatus == 0)
+    
+    system("ruby ../igc-kml.rb data/25GXXXX1.kml")
+    assert($?.exitstatus == 0)
+    
   end
 
 end
